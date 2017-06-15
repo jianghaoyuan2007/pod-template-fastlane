00e5763edb3be79588005cf9505c14bf07679f96
Stephen Chiang <jianghaoyuan2007@gmail.com>
2017-06-15 19:38:24 +0800

    Update pod-template files.

--- POD_NAMETests/POD_NAMETests.m
+++ POD_NAMETests/POD_NAMETests.m
@@ -1,6 +1,6 @@
 //
-//  POD_NAMETests.m
-//  POD_NAMETests
+//  ${POD_NAME}Tests.m
+//  ${POD_NAME}Tests
 //
 //  Created by Stephen Chiang on 14/06/2017.
 //
@@ -15,7 +15,7 @@ describe(@"Math", ^{
     it(@"is pretty cool", ^{
         NSUInteger a = 16;
         NSUInteger b = 26;
-        [[theValue(a + b) should] equal:theValue(43)];
+        [[theValue(a + b) should] equal:theValue(42)];
     });
 });
 
